7ad34cb03c8f31e83a3b0611ffa45e80d927937e
Kateme14 <katyuka.9114@gmail.com>
2024-02-11 21:35:17 +0100

    fourth and fifth tasks completed

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -21,3 +21,17 @@ console.log(`${count}`)
 
 console.log(parseInt(userName))
 console.log(Number(userName))
+
+let a = 1
+let b = 2
+let c = 'белых медведей'
+console.log(`${a}${b} ${c}`)
+
+let firstWord = "доступ"
+let secondWord = "морпех"
+let thirdWord = "наледь"
+let fourthWord = "попрек"
+let fifthWord = "рубило"
+let lengthWords = firstWord.length + secondWord.length + thirdWord.length + fourthWord.length + fifthWord.length
+
+console.log(lengthWords)
