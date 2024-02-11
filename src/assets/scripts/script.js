38a0dcad79330bdd40a7b33a276c2b9111abc679
Kateme14 <katyuka.9114@gmail.com>
2024-02-11 21:25:03 +0100

    third task completed

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -10,3 +10,14 @@ let days = 365
 let myAge = 32
 
 console.log(myAge * days * hours * minutes * seconds)
+
+let count = 42
+let userName = '42'
+
+console.log(count + '')
+console.log(String(count))
+console.log(count.toString())
+console.log(`${count}`)
+
+console.log(parseInt(userName))
+console.log(Number(userName))
