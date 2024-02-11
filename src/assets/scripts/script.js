e05cd2366681139af3ffe227f18f29af57b9e441
Kateme14 <katyuka.9114@gmail.com>
2024-02-11 21:20:15 +0100

    second task completed

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -2,3 +2,11 @@ let x = 20
 let y = 58 
 let z = 42
 console.log(x + y + z);
+
+let seconds = 60
+let minutes = 60
+let hours = 24
+let days = 365
+let myAge = 32
+
+console.log(myAge * days * hours * minutes * seconds)
