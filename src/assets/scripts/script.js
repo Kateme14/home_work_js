5fc14dcc419fa2a66aebcb15780b9b147b03fef1
Kateme14 <katyuka.9114@gmail.com>
2024-02-24 20:53:46 +0100

    first task completed

---
+++ src/assets/scripts/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let string = 'true'
+let boolean = false
+let number = 17
+let undefined = undefined
+let myNull = null
+
+console.log(typeof string)
+console.log(typeof boolean)
+console.log(typeof number)
+console.log(typeof undefined)
+console.log(typeof myNull)
