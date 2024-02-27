8aaba9a5ba1fa44c40c77c84c7387df3fa6d133c
Kateme14 <katyuka.9114@gmail.com>
2024-02-27 22:17:41 +0100

    third, fourth tasks completed

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -28,3 +28,30 @@ console.log(myCredit)
 
 let yourCredit = creditCalc(8900)
 console.log(yourCredit)
+
+// Задание 3
+
+function trimString (str, from, to){
+    let startIndex = str.indexOf(from)
+    let endIndex = str.indexOf(to)
+    if (startIndex === -1 || endIndex === -1 || endIndex<startIndex){
+        return str
+    }
+    return str.substring(startIndex+from.length, endIndex)
+}
+
+let stringInput = "Строка, которую я хочу обрезать"
+let resultString = trimString(stringInput, "Строка", "хочу")
+console.log(resultString)
+
+//Задание 4
+function sumOfDigits(number){
+    let sum1 = 0
+    let startPoint = String(number)
+    for(let i=0; i<startPoint.length; i++){
+        sum1+=Number(startPoint[i])
+    }
+    return sum1
+}
+
+console.log(sumOfDigits(9987))
