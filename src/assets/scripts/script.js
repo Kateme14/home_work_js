75454e28e8139bd587811f16cba9272dbece02df
Kateme14 <katyuka.9114@gmail.com>
2024-03-03 21:06:15 +0100

    second advanced task completed

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -127,3 +127,29 @@ function footballScore(score){
 }
 
 console.log(footballScore("4:10"))
+
+//Задание 2* 
+
+let student1 = {
+    name: 'Polina',
+    age: 27,
+    }
+let student2 = {
+    name: 'Polina',
+    age: 27,
+    }
+function compareObjects(obj1, obj2){
+    if(Object.keys(obj1).length !== Object.keys(obj2).length){
+    return false
+    }
+
+    for(let key in obj1){
+        if(obj1[key] !== obj2[key]){
+            return false
+        }
+    }
+
+    return true
+}
+
+console.log(compareObjects(student1, student2))
