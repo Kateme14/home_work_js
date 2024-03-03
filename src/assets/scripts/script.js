3bc72ed55d4ddfdb39eeb23c2fb21d931f1d3973
Kateme14 <katyuka.9114@gmail.com>
2024-03-03 18:44:13 +0100

    second task completed

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -11,3 +11,16 @@ delete myObject.name
 delete myObject.city
 
 console.log("После того как удалили:", myObject)
+
+//Задание 2
+
+let shopList = {
+    orange: 'yes',
+    cake: 'no'
+}
+
+if('cake' in shopList){
+    console.log('true')
+} else{
+    console.log('false')
+}
