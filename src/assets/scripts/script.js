a8b7d1b523a57c83f1bade800f46e4bd5c27f7b6
Kateme14 <katyuka.9114@gmail.com>
2024-03-05 21:45:07 +0100

    third task completed

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -10,3 +10,39 @@ console.log(colors.length)
 
 const animals = ['monkey', 'dog', 'cat']
 console.log(animals[animals.length-1])
+
+//Задание 3
+
+//Способ 1
+
+const numbers = [5, 43, 63, 23, 90]
+
+while(numbers.length > 0){
+    numbers.pop()
+}
+
+console.log(numbers)
+
+//Способ 2
+
+const numbers1 = [5, 43, 63, 23, 90]
+
+while(numbers1.length > 0){
+    numbers1.shift()
+}
+
+console.log(numbers1)
+
+//Способ 3
+
+const numbers2 = [5, 43, 63, 23, 90]
+numbers2.length = 0
+
+console.log(numbers2)
+
+//Способ 4
+
+const numbers3 = [5, 43, 63, 23, 90]
+numbers3.splice(0, numbers3.length)
+
+console.log(numbers3)
