26c42d7624e55f52d49791e4c4552b874e5a9d4d
Kateme14 <katyuka.9114@gmail.com>
2024-03-10 19:52:54 +0100

    second task completed

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -15,3 +15,26 @@ const fibonacci2 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
 
 fibonacci2.forEach(numbers1=>console.log(numbers1))
 
+
+//Задание 2
+//Способ 1
+
+const users = ['Darya', 'Masha', 'Denis', 'Vitaliy', 'Polina', 'Anton'];
+
+function formatUsers(user, index) {
+    return `member ${index + 1}: ${user}`;
+}
+
+const formattedUsers = users.map(formatUsers);
+
+console.log(formattedUsers);
+
+//Способ 2
+
+const users1 = ['Darya', 'Masha', 'Denis', 'Vitaliy', 'Polina', 'Anton'];
+
+const formattedUsers1 = users1.map((user, index) => `member ${index + 1}: ${user}`);
+
+console.log(formattedUsers1);
+
+
