ca2d6b6cac86e8ad8b7082c1961be8c0b43d145c
Kateme14 <katyuka.9114@gmail.com>
2024-03-10 19:26:31 +0100

    first task completed

---
+++ src/assets/scripts/script.js
@@ -0,0 +1,17 @@
+//Задание 1
+
+//Способ 1
+const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
+
+function allNumbers(numbers){
+    console.log(numbers)
+}
+
+fibonacci.forEach(allNumbers)
+
+//Способ 2
+
+const fibonacci2 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
+
+fibonacci2.forEach(numbers1=>console.log(numbers1))
+
